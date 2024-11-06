012505ad2460d5de0ef11808cedfdcef3c5d46b7
Saidazizhon <asrarovs26@gmail.com>
2024-11-06 09:40:04 +0500

    tugadi

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const SpotifyWebApi = require("spotify-web-api-node")
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post("/login", (req, res) => {
+  const code = req.body.code
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:5173",
+    clientId: "3e20d0ea8a004790ac45abb18f7d0193",
+    clientSecret: "4b416144592e4c55b38bd2828a42936e"
+  })
+  spotifyApi.authorizationCodeGrant(code).then(data => {
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in
+    })
+  }).catch(err => {
+    res.sendStatus(400)
+  })
+})
+
+app.listen(3001)
